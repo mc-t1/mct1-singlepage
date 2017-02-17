3f94f1824a8541ae39aff1fab930322ecde97cfa
Timothy Marwick <triyuga@gmail.com>
2017-02-17 20:45:08 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# insulin-engine
+Naive implementation of insulin and blood glucose interaction, for educational purposes.
+
+View the running engine implementation at https://magikcraft.github.io/insulin-engine/
+
+Pull Requests and feedback via Github issues are welcome!
+
+This engine integrates with a Minecraft plugin to provide a Virtual Reality experience of living with [Type 1 Diabetes](https://www.diabetesaustralia.com.au/type-1-diabetes) for newly diagnosed children and families to understand the interaction of carbs, insulin, and Blood Glucose Levels; to experience community with others living with the condition; and for their friends and communities to understand what they live with and how to support them.
+
+Project adviser [Scott Hanselman](http://www.hanselman.com/) is a software developer, Type 1 Diabetic, and activist. In this video Scott describes what it is like to live with Type 1 Diabetes:
+
+[![Scott Hanselman on Type 1 Diabetes](https://img.youtube.com/vi/jsxFbDIvQRw/0.jpg)](https://www.youtube.com/watch?v=jsxFbDIvQRw target="_blank")
+
+If you are a parent of a child diagnosed with Type 1 Diabetes in Queensland, join the [TOPIQ (Type One Parents In Queensland) Facebook group](https://www.facebook.com/groups/TOPIQ/) for a supportive community.
+
+This project was initiated as a [Landmark Self-Expression and Leadership Program](http://www.landmarkworldwide.com/advanced-programs/self-expression-and-leadership-program) project using the structures and coaching of the program, and is empowered by developers from [Magikcraft](http://www.magikcraft.io), with the support of [Diabetes Queensland](http://www.diabetesqld.org.au/).
+
+For more information on Type 1 Diabetes initiatives by technology teams around the world, see the [#WeAreNotWaiting hashtag on Twitter](https://twitter.com/search?q=%23WeAreNotWaiting&src=typd).
---
+++ js/mct1.js
@@ -0,0 +1,129 @@
+var dummyCarbsMap = {
+  "15" : "Bread",
+  "30" : "Watermelon",
+  "45" : "Cookie"
+}
+"use strict";
+var vue = new Vue({
+  el: "#app",
+  template: "#vueroot",
+  data: function(){
+    return {
+      configCarbsReferenceUnit: 15,
+      configMinSafeBGL: 4,
+      configMaxSafeBGL: 8,
+      configCriticalHighBGL: 20,
+      bloodGlucose: 6,
+      foodValue: 15,
+      foodUnits: 1,
+      insulinUnits: 1,
+      insulinUnitsInSystem: 0,
+      carbsInSystem: 0,
+      feedbackMessages: [],
+      playerHeartsValue: 10,
+      playerHeartsMax: 10,
+    }
+  },
+  created: function(){
+    console.log("created");
+  },
+  methods: {
+    eatFood: function(){
+      this.carbsInSystem += this.foodValue * this.foodUnits;
+      this.feedbackMessages.push(
+        "CMD RUN: /t1 eat " + this.foodUnits + " " + dummyCarbsMap[this.foodValue.toString()]
+        );
+      this.feedbackMessages.push(
+        "USR MSG: you ate " + this.foodUnits + " pieces of " + dummyCarbsMap[this.foodValue.toString()]
+        );
+
+      this.updateSimpleModel();
+    },
+    takeInsulin: function(){
+      this.insulinUnitsInSystem += this.insulinUnits;
+      this.feedbackMessages.push("CMD RUN: /t1 take " + this.insulinUnits + " insulin");
+
+      this.feedbackMessages.push(
+        "USR MSG: you took " + this.insulinUnits + " units of insulin"
+        );
+
+
+
+
+      this.updateSimpleModel();
+    },
+    updateSimpleModel: function(){
+      // increase bgl
+      msg_bgl_changes = "USR MSG: Your stomach digests " + this.carbsInSystem;
+      this.bloodGlucose += (
+          this.carbsInSystem * 0.25
+        );
+      if(this.carbsInSystem > 0)
+        this.feedbackMessages.push(
+            msg_bgl_changes + " which raises you blood glucose level to " + this.bloodGlucose
+          );
+        this.feedbackMessages.push(
+            "USR MSG: The equation was: BGL = BGL + (carbs in system * 0.25)"
+          );
+      this.carbsInSystem = 0;
+
+
+      // decrease bgl by amount of insulin in system, to a maximum of zero
+
+      var toAbsorb = this.insulinUnitsInSystem * this.bglReferenceUnit();
+
+      toAbsorbDiffBGL = this.bloodGlucose - toAbsorb;
+      actualAbsorbedInsulin = 0;
+
+      if(toAbsorbDiffBGL < 0){
+        actualAbsorbedInsulin
+          = this.insulinUnitsInSystem - Math.abs(toAbsorbDiffBGL/this.bglReferenceUnit());
+      }else{
+        actualAbsorbedInsulin = this.insulinUnitsInSystem;
+      }
+      this.bloodGlucose -= actualAbsorbedInsulin * this.bglReferenceUnit();
+
+      this.insulinUnitsInSystem -= actualAbsorbedInsulin;
+
+      if(actualAbsorbedInsulin > 0){
+        this.feedbackMessages.push(
+             "USR MSG: " + actualAbsorbedInsulin + " units of insulin were used to process your blood glucose"
+          );
+        this.feedbackMessages.push("USR MSG: You gained health");
+      }else {
+        this.feedbackMessages.push("USR MSG: No blood glucose was processed into health.");
+      }
+
+      this.feedbackMessages.push(
+        "USR MSG: You have " + this.insulinUnitsInSystem +" insulin in your system"
+      );
+
+
+
+
+      // we can't have negative blood glucose, it is really just unused (excess) insulin
+      // update insulin & blood glucose to show this
+
+      //finished making changes to player t1 stats
+      //what does this mean for player health / side effects
+      this.applySideEffects();
+    },
+    bglReferenceUnit: function(){
+      return this.configCarbsReferenceUnit * 0.25;
+    },
+    applySideEffects: function(){
+      if(this.bloodGlucose < this.configMinSafeBGL){
+        this.feedbackMessages.push("USR MSG: Hypoglycemic shock. My player dies without immediate emergency medical intervention.")
+      }
+      if(this.bloodGlucose > this.configMaxSafeBGL){
+        if (this.bloodGlucose > this.configCriticalHighBGL) {
+          this.feedbackMessages.push("USR MSG: Diabetic Ketoacidosis Attack (DKA). Without medical intervention my player loses consciousness and will die.");
+        } else
+        this.feedbackMessages.push("USR MSG: Hyperglycemia. My BGL is too high (> "+ this.configMaxSafeBGL+") not enough insulin! It causes damage to my player health");
+      }
+    },
+    clearFeedback: function(){
+      this.feedbackMessages = [];
+    }
+  }
+});
