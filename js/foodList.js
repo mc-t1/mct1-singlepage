687980ca09dc1f0d4c796a27c3516c5934e0d524
Isaac <isaachill13@gmail.com>
2017-02-18 14:55:12 +1000

    food list

--- js/foodList.js
+++ js/foodList.js
@@ -4,13 +4,13 @@ function foodList() {
       1 : {
         'foodValue' : '10',
         'carbsValue' : '2',
-        'image':'Apple2.png',
+        'image':'./img/food/Apple2.png',
         'name':'Apple'
       },
       2 : {
         'foodValue' : '5',
         'carbsValue' : '5',
-        'image':'Carrot.png',
+        'image':'./img/food/Carrot.png',
         'name':'Carrot'
       }
     }
